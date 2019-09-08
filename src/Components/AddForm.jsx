d374cd3db1006a2da604d80acc95ff0e0f5d640e
Spencer Rhoton <rhotonspencer@gmail.com>
2019-09-07 19:25:22 -0600

    finished add-box and worked on styleing. Weather component needs to be finished

--- src/Components/AddForm.jsx
+++ src/Components/AddForm.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import '../App.css'
 import addImg from '../assets/plus.png'
+import cancelImg from '../assets/cancel.png'
+import submitImg from '../assets/submit.png'
 
 export default class App extends Component {
     constructor(props) {
@@ -55,24 +57,52 @@ export default class App extends Component {
                 <div>
                     {!this.state.add ? 
                         <div className="button-holder">
-                            <img src={addImg} onClick={this.toggleAdd} alt=""/>              
+                            <img src={addImg} onClick={this.toggleAdd} alt="Add Button Icon"/>              
                         </div> 
                         : 
-                        <div className="add-holder">
-                            <input name="nameInput" onChange={(e) => this.handleChange(e)} placeholder="Resort Name" type="text"/>
-                            <input name="cityInput" onChange={(e) => this.handleChange(e)} placeholder="City"type="text"/>
-                            <input name="stateInput" onChange={(e) => this.handleChange(e)} placeholder="State" type="text"/>
-                            <input name="countryInput" onChange={(e) => this.handleChange(e)} placeholder="Country" type="text"/>
-                            <input name="zipInput" onChange={(e) => this.handleChange(e)} placeholder="Zip Code" type="text"/>
-                            <input name="openingDateInput" onChange={(e) => this.handleChange(e)} placeholder="Opening Date" type="text"/>
-                            <input name="liftsOpenInput" onChange={(e) => this.handleChange(e)} placeholder="Lifts Open" type="text"/>
-                            <input name="totalLiftsInput" onChange={(e) => this.handleChange(e)} placeholder="Total Lifts" type="text"/>
-                            <input name="trailsOpenInput" onChange={(e) => this.handleChange(e)} placeholder="Trails Open" type="text"/>
-                            <input name="totalTrailsInput" onChange={(e) => this.handleChange(e)} placeholder="Total Trails" type="text"/>
-                            <input name="parksOpenInput" onChange={(e) => this.handleChange(e)} placeholder="Parks Open" type="text"/>
-                            <input name="totalParksInput" onChange={(e) => this.handleChange(e)} placeholder="Total Parks" type="text"/>
-                            <button onClick={this.toggleAdd}>Cancel</button>
-                            <button onClick={this.addResort}>Submit</button>
+                        <div className="add-form-holder">
+                            <div className="add-form-box">
+                                <div className="add-text-and-input-holder">
+                                    <div className="add-form-text">
+                                        <h3>Resort Name: </h3>
+                                        <h3>City: </h3>
+                                        <h3>State: </h3>
+                                        <h3>Country: </h3>
+                                        <h3>Zip Code: </h3>
+                                        <h3>Opening Date: </h3>
+                                        <h3>Lifts Open: </h3>
+                                        <h3>Total Lifts: </h3>
+                                        <h3>Trails Open: </h3>
+                                        <h3>Total Trails: </h3>
+                                        <h3>Parks Open: </h3>
+                                        <h3>Total Parks: </h3>
+                                    </div>
+                                    <div className="add-form-input">
+                                        <input className="add-input"name="nameInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="cityInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="stateInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="countryInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="zipInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="openingDateInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="liftsOpenInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="totalLiftsInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="trailsOpenInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="totalTrailsInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="parksOpenInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                        <input name="totalParksInput" onChange={(e) => this.handleChange(e)} type="text"/>
+                                    </div>
+
+                                </div>
+                                <div className="add-button-holder">
+                                    {/* <button onClick={() => this.toggleEdit()}>Cancel</button> */}
+                                    <img src={cancelImg} onClick={this.toggleAdd} alt="Cancel Button Icon"/>
+                                    {/* <button onClick={this.editResort}>Submit</button> */}
+                                    <img src={submitImg}  onClick={this.addResort} alt="Submit Button Icon"/>
+                                </div>
+
+                            </div>
+                                {/* <button onClick={this.toggleAdd}>Cancel</button>
+                                <button onClick={this.addResort}>Submit</button> */}
                         </div>
                 }
                 </div>
